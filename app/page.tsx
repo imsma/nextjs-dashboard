8f6fcc1ccbd891fa0de5115d521a45f980268f32
SMA <sma@wizlinx.com>
2024-06-18 21:47:46 +0500

    image optimization + layout + links

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -33,6 +34,21 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshot of the dashboard project showingg desktop version"
+          />
+
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showingg mobile version"
+          />
         </div>
       </div>
     </main>
