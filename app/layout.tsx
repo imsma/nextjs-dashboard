fdac51242223d1e68613d630424d8f0724ab824e
SMA <sma@wizlinx.com>
2024-06-25 01:16:09 +0500

    Creating the login route and seo metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Dashboard bySMA',
+    default: 'Dashboard bySMA',
+  },
+  description: 'Next.Js dashboard bySMA',
+  metadataBase: new URL('https://sma.im'),
+};
+
 export default function RootLayout({
   children,
 }: {
